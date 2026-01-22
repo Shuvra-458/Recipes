5f14ae561a8b6acf8c56c80bac1ec761eeef4d5d
Shuvrajyoti <shuvra458@gmail.com>
2026-01-23 03:29:52 +0530

    Configure CORS settings in main.py
    Added CORS settings to allow all origins, methods, and headers.

--- app/main.py
+++ app/main.py
@@ -15,7 +15,10 @@ app.add_middleware(
         "http://127.0.0.1:5500",
         "*",
         "null",
-    ]
+    ],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
 
 )
 
