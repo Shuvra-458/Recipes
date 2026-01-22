187aae0532165632fc55e4617b4e23e4cee030ad
Shuvrajyoti <shuvra458@gmail.com>
2026-01-23 03:38:15 +0530

    Simplify CORS settings to allow all origins

--- app/main.py
+++ app/main.py
@@ -1,30 +1,19 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.database import engine
-from app import models
 from app.routes import recipes
 
 app = FastAPI(title="Recipes API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost",
-        "http://127.0.0.1",
-        "http://localhost:5500",
-        "http://127.0.0.1:5500",
-        "*",
-        "null",
-    ],
-    allow_credentials=False,
+    allow_origins=["*"],          
+    allow_credentials=False,      
     allow_methods=["*"],
     allow_headers=["*"],
-
 )
 
 app.include_router(recipes.router)
 
-models.Base.metadata.create_all(bind=engine)
 
 @app.get("/health")
 def health_check():
