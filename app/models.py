7a3eca13baf39474daca55e068d5e879663a4c2e
Shuvrajyoti <shuvra458@gmail.com>
2026-01-23 11:08:26 +0530

    Adding leftout fields

--- app/models.py
+++ app/models.py
@@ -6,6 +6,8 @@ class Recipe(Base):
     __tablename__ = "recipes"
 
     id = Column(Integer, primary_key=True, index=True)
+    continent = Column(String(100))
+    country_state = Column(String(100))
     cuisine = Column(String(255))
     title = Column(String(255), index=True)
     rating = Column(Float, nullable=True)
@@ -16,3 +18,4 @@ class Recipe(Base):
     nutrients = Column(JSONB)
     instructions = Column(JSONB)
     serves = Column(String(100))
+    source_url = Column(Text)
