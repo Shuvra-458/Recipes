d47ac53d525b61acc0a7e04e975a9aa65f9acbe0
Shuvrajyoti <shuvra458@gmail.com>
2026-01-23 11:09:33 +0530

    Add optional fields to RecipeResponse schema

--- app/schemas.py
+++ app/schemas.py
@@ -3,6 +3,9 @@ from typing import Optional, Dict, Any, List
 
 class RecipeResponse(BaseModel):
     id: int
+    continent: Optional[str]
+    country_state: Optional[str]
+    source_url: Optional[str]
     title: Optional[str]
     cuisine: Optional[str]
     rating: Optional[float]
