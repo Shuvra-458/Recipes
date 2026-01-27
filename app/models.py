58022c2792a75709e919fb5bf37803fbb33b868e
Shuvrajyoti <shuvra458@gmail.com>
2026-01-27 20:49:13 +0530

    Add comments to Recipe model for clarity
    Added comments to clarify table structure and column properties.

--- app/models.py
+++ app/models.py
@@ -2,15 +2,16 @@ from sqlalchemy import Column, Integer, String, Float, Text
 from sqlalchemy.dialects.postgresql import JSONB
 from app.database import Base
 
+#Creates the table in the Postgres Database
 class Recipe(Base):
-    __tablename__ = "recipes"
+    __tablename__ = "recipes" #Name of the table
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True) 
     continent = Column(String(100))
-    country_state = Column(String(100))
+    country_state = Column(String(100)) 
     cuisine = Column(String(255))
-    title = Column(String(255), index=True)
-    rating = Column(Float, nullable=True)
+    title = Column(String(255), index=True) #index=true adds index to the title column, which helps in improving the querying performance
+    rating = Column(Float, nullable=True)  #nullable=True allows for null value from the json data to be inserted in the columns
     prep_time = Column(Integer, nullable=True)
     cook_time = Column(Integer, nullable=True)
     total_time = Column(Integer, nullable=True)
